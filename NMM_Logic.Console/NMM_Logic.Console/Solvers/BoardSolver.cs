57a9c8eb4f3f8439db0a3af6ab34a80e28aa687b
A3Tom <Tom_Esq@Yahoo.com>
2023-11-21 23:49:03 +0000

    Potential abstract for solvers

---
+++ NMM_Logic.Console/NMM_Logic.Console/Solvers/BoardSolver.cs
@@ -0,0 +1,9 @@
+﻿using NMM_Logic.Console.Classes;
+
+namespace NMM_Logic.Console.Solvers;
+internal abstract class BoardSolver
+{
+    internal abstract bool IsValidMove(BoardState boardState, byte[] destination);
+    internal abstract bool MoveTile(BoardState board, byte[] source, byte[] destination);
+    internal abstract bool HasTriggeredRemovalPhase(byte[] destination);
+}
