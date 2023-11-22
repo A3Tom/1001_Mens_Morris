1a804d0ca26430586c3b645b32c316fe0b603fa2
A3Tom <Tom_Esq@Yahoo.com>
2023-11-22 01:29:10 +0000

    Using flags instead of byte arrays to store board positions

--- NMM_Logic.Console/NMM_Logic.Console/Solvers/BoardSolver.cs
+++ NMM_Logic.Console/NMM_Logic.Console/Solvers/BoardSolver.cs
@@ -3,7 +3,7 @@
 namespace NMM_Logic.Console.Solvers;
 internal abstract class BoardSolver
 {
-    internal abstract bool IsValidMove(BoardState boardState, byte[] destination);
-    internal abstract bool MoveTile(BoardState board, byte[] source, byte[] destination);
-    internal abstract bool HasTriggeredRemovalPhase(byte[] destination);
+    internal abstract bool IsValidMove(BoardState boardState, BoardPosition destination);
+    internal abstract bool MoveTile(BoardState board, BoardPosition source, BoardPosition destination);
+    internal abstract bool HasTriggeredRemovalPhase(BoardPosition currentPositions, BoardPosition destination);
 }
