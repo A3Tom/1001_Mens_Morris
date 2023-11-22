6d18820a2313c4dbb9ae3064558a86d1c34e2da9
A3Tom <Tom_Esq@Yahoo.com>
2023-11-22 01:33:49 +0000

    Added console for testing

--- NMM_Logic.Console/NMM_Logic.Console/Program.cs
+++ NMM_Logic.Console/NMM_Logic.Console/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*****************
+ * 0-----0-----0
+ * --0---0---0--
+ * ----0-0-0----
+ * 0-0-0---0-0-0
+ * ----0-0-0----
+ * --0---0---0--
+ * 0-----0-----0
+ *****************/
+
+using NMM_Logic.Console.Classes;
+using NMM_Logic.Console.Solvers;
+
+var solver = new MovingTileSolver();
+
+var currentPositions = 
+    BoardPosition.Upper_Top_Left | 
+    BoardPosition.Upper_Top_Middle | 
+    BoardPosition.Upper_Mid_Left | 
+    BoardPosition.Upper_Mid_Right | 
+    BoardPosition.Right_Mid_Far;
+
+var destination = BoardPosition.Upper_Top_Right;
+
+var hasTriggeredRemovalPhase = solver.HasTriggeredRemovalPhase(currentPositions, destination);
+
+
+Console.WriteLine($"A cheeky wee slide tae {destination} has {(hasTriggeredRemovalPhase ? string.Empty : "Not ")}triggered a removal phase");
