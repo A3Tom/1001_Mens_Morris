663a2b8790a905359801df73d32a1b12e903d8c6
A3Tom <Tom_Esq@Yahoo.com>
2023-11-23 22:19:43 +0000

    Added clear call for cli

--- NMM_Logic.Console/NMM_Logic.Console/Classes/AsciiBoardDisplayer.cs
+++ NMM_Logic.Console/NMM_Logic.Console/Classes/AsciiBoardDisplayer.cs
@@ -22,6 +22,7 @@ internal static class AsciiBoardDisplayer
 {
     public static void OutputToCLI(BoardState boardState)
     {
+        Console.Clear();
         Console.OutputEncoding = System.Text.Encoding.UTF8;
 
         OutputUpperTopLine(boardState.PlayerPositions);
