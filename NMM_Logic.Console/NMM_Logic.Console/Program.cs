3646f9f25c73df50e01d26b4373c6904eec01d4f
A3Tom <Tom_Esq@Yahoo.com>
2023-11-23 19:08:45 +0000

    Updated program.cs, kinda just usin this for a testin hing atm

--- NMM_Logic.Console/NMM_Logic.Console/Program.cs
+++ NMM_Logic.Console/NMM_Logic.Console/Program.cs
@@ -17,26 +17,17 @@
 */
 
 using NMM_Logic.CLI.Classes;
-using NMM_Logic.Console.Classes;
-using NMM_Logic.Console.Solvers;
+using NMM_Logic.CLI.Solvers;
 
 var solver = new MovingTileSolver();
 
 var boardState = new BoardState(solver);
 
+boardState.MoveTile(BoardPosition.Upper_Top_Left);
+boardState.MoveTile(BoardPosition.Upper_Mid_Left);
+boardState.MoveTile(BoardPosition.Upper_Top_Right);
+boardState.MoveTile(BoardPosition.Upper_Mid_Right);
 
-var currentPositions = 
-    BoardPosition.Upper_Top_Left | 
-    BoardPosition.Upper_Top_Middle | 
-    BoardPosition.Upper_Mid_Left | 
-    BoardPosition.Upper_Mid_Right | 
-    BoardPosition.Right_Mid_Far;
+AsciiBoardDisplayer.OutputToCLI(boardState);
 
-var destination = BoardPosition.Upper_Bottom_Right;
-
-boardState.MoveTile(destination);
-
-var hasTriggeredRemovalPhase = solver.HasTriggeredRemovalPhase(currentPositions, destination);
-
-
-Console.WriteLine($"A cheeky wee slide tae {destination} has {(hasTriggeredRemovalPhase ? string.Empty : "Not ")}triggered a removal phase");
+Console.ReadLine();
